0045da00b449ba865bc100b53cb40d202c9fed40
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2024-10-29 13:10:07 +0530

    try

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import * as serviceWorker from "./serviceWorker";
 if (environment.production) {
   enableProdMode();
 }
-
+serviceWorker.unregister();
 platformBrowserDynamic().bootstrapModule(AppModule);
