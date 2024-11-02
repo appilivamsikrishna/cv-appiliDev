95de788fffbce32e582b31d567b0648ec7f3bd33
Krishna <appilivamsikrishna@gmail.com>
2024-11-02 16:57:01 +0530

    inject

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { inject } from "@vercel/analytics";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as serviceWorker from "./serviceWorker";
@@ -8,4 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 serviceWorker.unregister();
+inject();
 platformBrowserDynamic().bootstrapModule(AppModule);
