4058ad13a3de2f437011a9fbbef7fb45969e81c3
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2024-11-13 23:55:48 +0530

    tyh

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -7,7 +7,7 @@ declare const bootstrap: any; // global reference
  */
 export class AppConstants {
 
-  public static DATE = '13 Nov 2024';
+  public static DATE = '14 Nov 2024';
   public static APP_TITLE = 'Krishna';
   public static AUTHOR_NAME = 'Appili Vamsi Krishna';
   public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE + ' - ' + AppConstants.DATE;
